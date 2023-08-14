8ae7758bb6396ec4128bda2d22a44f9a838aaf76
Terrence Shi <terrenceshi@gmail.com>
2023-08-14 02:45:55 -0400

    can load models!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from "@react-three/drei";
+
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+import { useFBX } from "@react-three/drei";
+
+function Box() {
+  return (
+    <mesh>
+      <boxBufferGeometry attach = "geometry" />
+      <meshLambertMaterial attach = "material" color = "hotpink" />
+    </mesh>
+  );
+}
 
 function App() {
+
+  const monsterFbx = useFBX('./models/mon.fbx');
+
+  const astroFbx = useFBX('./models/atest5.fbx');
+
+  const robberFbx = useFBX('./models/test.fbx');
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Canvas style = {{height: "100vh", background: "#171717"}}>
+        <OrbitControls />
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[1, 1, 3]}/>
+
+        <primitive object={monsterFbx} position = {[-1.5,-1,0]} scale = {0.03}/>
+        <primitive object={astroFbx} position = {[1.5,-1,0]} scale = {0.11}/>
+        <primitive object={robberFbx} position = {[0,-1,0]} scale = {0.02}/>
+
+        <mesh position={[1, 1, -1]}>
+          <boxGeometry />
+          <meshLambertMaterial attach = "material" color = "hotpink" />
+        </mesh>
+
+      </Canvas>
     </div>
   );
 }
